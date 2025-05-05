cc28949e26267555945ce719de190a43b5121119
SalomonAquije <76548579@certus.edu.pe>
2025-05-04 18:30:01 -0600

    Trabajo Presentacion Domingo 04-05

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Bienvenido al mundo de los relojes")
+
+const btnCart = document.querySelector(".container-icon")
+
+const containerCartProducts = document.querySelector(" .container-cart-products ")
+
+btnCart.addEventListener("click", () =>{
+    containerCartProducts.classList.toggle("hidden-cart")
+})
